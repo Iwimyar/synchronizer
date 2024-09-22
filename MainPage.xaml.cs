50eeabeb1860f8a1c74f8cea4b39f4317a6d61a3
ROOT <ROOT@DESKTOP-L98M56E>
2024-09-22 20:05:19 +0300

    Добавьте файлы проекта.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿using CommunityToolkit.Maui.Views;
+using synchronizer.Models;
+
+namespace synchronizer
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            BindingContext = new VideoContext();
+        }
+
+        public async Task<FileResult?> PickAndShow()
+        {
+            PickOptions options = new()
+            {
+                PickerTitle = "Please select a comic file",
+                FileTypes = FilePickerFileType.Videos,
+            };
+
+            try
+            {
+                return await FilePicker.Default.PickAsync(options);
+            }
+            catch (Exception ex)
+            {
+                // The user canceled or something went wrong
+            }
+
+            return null;
+        }
+
+        private async void Open_Clicked(object sender, EventArgs e)
+        {
+            FileResult? videoFile = await PickAndShow();
+
+            if (videoFile is null)
+                return;
+
+            ((VideoContext)BindingContext).FileSource = MediaSource.FromFile(videoFile.FullPath);
+        }
+    }
+
+}
---
+++ Models/VideoContext.cs
@@ -0,0 +1,36 @@
+﻿using CommunityToolkit.Maui.Views;
+using System.ComponentModel;
+
+namespace synchronizer.Models
+{
+    public class VideoContext : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        private MediaSource? _fileSource;
+
+        public MediaSource? FileSource 
+        {
+            get => _fileSource;
+            set
+            {
+                _fileSource = value;
+                OnPropertyChanged(nameof(FileSource));
+                OnPropertyChanged(nameof(FileChosen));
+            }
+        }
+
+        public bool FileChosen
+        {
+            get
+            {
+                return FileSource is not null;
+            }
+        }
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
